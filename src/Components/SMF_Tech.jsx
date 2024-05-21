d8a4cc2a5e6a7e7b5bd71ce5e3e842e9c300169d
Abanoub Samir <abanoubsamirshenouda@gmail.com>
2024-05-21 03:20:30 +0300

    SMF Tech.

--- src/Components/SMF_Tech.jsx
+++ src/Components/SMF_Tech.jsx
@@ -8,12 +8,13 @@ import {
 import { useNavigate } from "react-router-dom";
 import smfTechLogo from "../assets/images/brand/smftech.png";
 import smfTechTypo from "../assets/images/brand/smftecttypo.png";
+import mark from "../assets/images/mark.png";
 const gradient = {
   background: "radial-gradient(circle at 50% 50%, #8255f1, #0d2486)",
 };
 const SMF_Tech = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
     <>
       <div
@@ -23,31 +24,61 @@ const SMF_Tech = () => {
       >
         <img src={smfTechLogo} alt="" style={{ height: "100px" }} />
         <img src={smfTechTypo} alt="" style={{ height: "50px" }} />
-
       </div>
-      <Modal backdrop={"blur"} dir="ltr" isOpen={isOpen} onClose={onClose}>
+      <Modal
+        shadow="lg"
+        size="full"
+        backdrop={"blur"}
+        dir="ltr"
+        isOpen={isOpen}
+        onClose={onClose}
+      >
         <ModalContent>
           {() => (
             <>
-              <ModalBody style={gradient} className="rounded-4xl">
-                <img src={smfTechTypo} className="w-[70%] m-auto" alt="" />
+              <ModalBody
+                style={gradient}
+                className="rounded-4xl overflow-y-scroll"
+              >
+                <div className="grid grid-cols-2 items-center">
+                  <img src={smfTechTypo} className="w-[70%] m-auto" alt="" />
+                  <img src={mark} className="w-[70%] m-auto" alt="" />
+                </div>
+                <p className="text-3xl font-bold text-center text-white">
+                  احنا شباب مارمرقس
+                </p>
+                <p className="text-center text-xl text-white ">
+                  {" "}
+                  احنا اول خدمة متخصصة في التكنولوجيا و البرمجيات في الكنيسة
+                  القبطية
+                </p>
+                <p className="text-center text-xl text-white">
+                  {" "}
+                  هدفنا اننا نصنع بنفسنا التطوير الرقمي اللي هيعمل تجربة مختلفة
+                  و جديدة لكنيستنا القبطية و الخدمات الخاصة بيها{" "}
+                </p>
                 <div className="grid grid-cols-2 gap-5">
-                  <div onClick={()=> navigate('/team')} className="hover:bg-white hover:text-black my-5 transition-all duration-500 text-white h-[100px] border-1 flex justify-center items-center rounded-lg">
-                    <p className="text-3xl">About us </p>
+                  <div
+                    onClick={() => navigate("/team")}
+                    className="hover:bg-white hover:text-black my-5 transition-all duration-500 text-white h-[100px] border-1 flex justify-center items-center rounded-lg"
+                  >
+                    <p className="text-3xl">الفريق </p>
                   </div>
-                  <div onClick={()=>window.open('https://wa.me/201201891349' , '_blank')} className="hover:bg-white hover:text-black my-5 transition-all duration-500 text-white h-[100px] border-1 flex justify-center items-center rounded-lg">
-                    <p className="text-3xl">Contact </p>
+                  <div
+                    onClick={() =>
+                      window.open("https://wa.me/201201891349", "_blank")
+                    }
+                    className="hover:bg-white hover:text-black my-5 transition-all duration-500 text-white h-[100px] border-1 flex justify-center items-center rounded-lg"
+                  >
+                    <p className="text-3xl">شات </p>
                   </div>
                 </div>
-                  <div className="hover:bg-white w-full hover:text-black my-5 transition-all duration-500 text-white h-[100px] border-1 flex justify-center items-center rounded-lg">
-                    <a
-                      className="text-3xl"
-                      href="tel:201201891349"
-                    >
-                      Technical Support
-                    </a>
-                    <LinkIcon />
-                  </div>
+                <div className="hover:bg-white w-full hover:text-black my-5 transition-all duration-500 text-white h-[100px] border-1 flex justify-center items-center rounded-lg">
+                  <a className="text-3xl" href="tel:201201891349">
+                    الدعم الفني
+                  </a>
+                  <LinkIcon />
+                </div>
               </ModalBody>
             </>
           )}
