34287a9116daf6d44e082188f8968d08b4c1385d
Abanoub Samir <abanoubsamirshenouda@gmail.com>
2024-06-02 18:58:50 +0300

    delete 3d

--- src/App.jsx
+++ src/App.jsx
@@ -67,13 +67,13 @@ function App() {
               },
             }}
           />
-          <div>
+          {/* <div>
             <spline-viewer
               hint
               loading-anim-type="spinner-small-dark"
               url="https://prod.spline.design/FPtgk3R-fev4Sw3o/scene.splinecode"
             ></spline-viewer>
-          </div>
+          </div> */}
           <Suspense fallback={<Full_Screen_Skeleton_Loader />}>
             <Routes>
               <Route path="/" element={<Home />} />
