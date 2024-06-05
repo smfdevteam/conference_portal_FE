d8ffd1939af52d9e2cb3ec7b7639ce41928bab78
Abanoub Samir <abanoubsamirshenouda@gmail.com>
2024-06-05 03:05:19 +0300

    add speakers

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,13 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   open: true,
-  //   hmr: {
-  //     overlay: false,
-  //   },
-  // },
-  // optimizeDeps: {
-  //   include: ["react", "react-dom"],
-  // },
+  server: {
+    open: true,
+    hmr: {
+      overlay: false,
+    },
+  },
+  optimizeDeps: {
+    include: ["react", "react-dom"],
+  },
 });
