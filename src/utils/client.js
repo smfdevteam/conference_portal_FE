87399bb696e089d2af810f3c492472281f59141f
Abanoub Samir <abanoubsamirshenouda@gmail.com>
2024-07-06 20:18:12 +0300

    add manifest

--- src/utils/client.js
+++ src/utils/client.js
@@ -24,5 +24,6 @@ export const handleNotifications = async () => {
       await tokenHandler();
     }
   } catch (e) {
+    console.log(e.message)
   }
 };
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,57 @@
-import { defineConfig } from "vite";
-import {VitePWA} from 'vite-plugin-pwa'
 import react from "@vitejs/plugin-react-swc";
-import envCompatible from 'vite-plugin-env-compatible';
-// https://vitejs.dev/config/
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+const manifestForPlugIn = {
+  registerType: "prompt",
+  includeAssests: ["favicon.ico", "apple-touc-icon.png", "masked-icon.svg"],
+  manifest: {
+    name: "SMF Tech.",
+    short_name: "SMF Tech.",
+    description: "Conference Application",
+    icons: [
+      {
+        src: "/smf.png",
+        sizes: "192x192",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/smf.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "favicon",
+      },
+      {
+        src: "/smf.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/smf.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#f0e7db",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
 export default defineConfig({
-  plugins: [react() , envCompatible() , VitePWA({
-    registerType :'autoUpdate'
-  })],
+  plugins: [react()  , VitePWA(manifestForPlugIn)],
   server: {
     open: true,
     hmr: {
       overlay: false,
     },
+  },
+  build: {
+    manifest: true,
   },
   optimizeDeps: {
     include: ["react", "react-dom"],
