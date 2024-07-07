b6de2ae1f274690e575a8d4be900e06614c8be20
Abanoub Samir <abanoubsamirshenouda@gmail.com>
2024-07-07 05:27:09 +0300

    compress code

--- src/utils/client.js
+++ src/utils/client.js
@@ -7,20 +7,26 @@ export const isMobile = () => {
   );
 };
 const tokenHandler = async () => {
+  console.log('start token handler')
   if (!localStorage.getItem("notification_token")) {
+  console.log('start token handler inside if')
+
     const token = await getToken(CONFERENCE_FIREBASE_MESSAGEING_HANDLER, {
       vapidKey:
         "BHRkWHqZNQekKcfTwmVEXf2gvgUQ3Rs7N0YddvTTYHLALeLHmf3ghPZGYMkBGzd5CHqMNDnQKF18RcfFkKYTj3I",
     });
+    console.log('token' , token)
     localStorage.setItem("notification_token", token);
   }
 };
 export const handleNotifications = async () => {
   try {
     if (Notification.permission !== "granted") {
+      console.log('not grandted')
       await Notification.requestPermission();
       await tokenHandler();
     } else {
+      console.log('grandted')
       await tokenHandler();
     }
   } catch (e) {
