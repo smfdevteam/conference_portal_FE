d61d92c0dfa12c2050028c54c4fa79b76dede05c
Abanoub Samir <abanoubsamirshenouda@gmail.com>
2024-07-12 06:11:24 +0300

    add some dala3

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,13 +9,15 @@ import Request_Help from "../../Components/help/Request_Help";
 import User_Card from "../../Components/user/User_Card";
 import { stateProvider } from "../../Context/App_Context";
 import SMFTech1 from "../../assets/images/brand/smftech.png";
+import SMFTechTypo from "../../assets/images/brand/smftecttypo.png";
 import SMFTech2 from "../../assets/images/brand/familyLogo-removebg-preview (1).png";
 import leadersBadge from "../../assets/images/icons/badge.png";
 import { Divider, Image } from "@nextui-org/react";
+import { Avatar } from "@nextui-org/react";
 export default function Home() {
   const {
     app_state: {
-      user: { isLeader, pointId, points, displayName },
+      user: { isLeader, displayName, photoURL },
       conference: { name },
     },
   } = useContext(stateProvider);
@@ -44,22 +46,48 @@ export default function Home() {
         <Now now={homeState.now} />
       </div>
       {isLeader && (
-        <div className="shadow-lg bg-black border-3 rounded-2xl py-2 px-5 my-3">
-          <p className="text-white text-center">
-            Leader /<span className="text-3xl mx-3">{displayName.split(" ")[0]}</span>
-          </p>
-          <div className="grid grid-cols-2 gap-3 ">
-            <Leader_badge label={"Teams"} route={"/points"} />
-            <Leader_badge label={"Members"} route={"/points-guest"} />
-          </div>
-          <div className="flex items-center justify-center gap-5">
-            <img src={leadersBadge} width={20} />
-            <p className="text-white text-center capitalize">
-              You Now Own the Leaders Badge
+        <>
+          <div className="shadow-lg bg-black border-3 rounded-2xl py-2  my-3 flex justify-center items-center">
+            <Avatar
+              imgProps={{
+                style: {
+                  objectFit: "cover",
+                  width: "100%",
+                  height: "100%",
+                },
+              }}
+              isBordered
+              color="secondary"
+              src={photoURL}
+            />
+            <p className="text-white text-center capitalize mx-4">
+              <p>SMF Tech. trusts you</p>
+              <p>!We've got your back , {displayName.split(" ")[0]}</p>
             </p>
-            <img src={leadersBadge} width={20} />
+            <Avatar
+              imgProps={{
+                style: {
+                  objectFit: "cover",
+                  width: "200%",
+                  height: "200%",
+                },
+              }}
+              isBordered
+              color="secondary"
+              src={SMFTech1}
+            />
           </div>
-        </div>
+          <div className="shadow-lg bg-black border-3 rounded-2xl py-2 px-5 my-3">
+            <p className="text-white text-center">
+              Leader /
+              <span className="text-3xl mx-3">{displayName.split(" ")[0]}</span>
+            </p>
+            <div className="grid grid-cols-2 gap-3 ">
+              <Leader_badge label={"Teams"} route={"/points"} />
+              <Leader_badge label={"Members"} route={"/points-guest"} />
+            </div>
+          </div>
+        </>
       )}
       <SMF_Tech />
       <User_Card />
